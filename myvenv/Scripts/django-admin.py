d1270e40cee71e37c11df8d9ffc8a98946af796a
gowtham <gowthamjanga7@gmail.com>
2024-12-07 17:40:12 +0530

    adding template project

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,21 @@
+#!C:\Users\gowth\OneDrive\Desktop\python_django_rest_job_preparation\2.django\django_revision\templateproject\myvenv\Scripts\python.exe
+# When the django-admin.py deprecation ends, remove this script.
+import warnings
+
+from django.core import management
+
+try:
+    from django.utils.deprecation import RemovedInDjango40Warning
+except ImportError:
+    raise ImportError(
+        'django-admin.py was deprecated in Django 3.1 and removed in Django '
+        '4.0. Please manually remove this script from your virtual environment '
+        'and use django-admin instead.'
+    )
+
+if __name__ == "__main__":
+    warnings.warn(
+        'django-admin.py is deprecated in favor of django-admin.',
+        RemovedInDjango40Warning,
+    )
+    management.execute_from_command_line()
---
+++ templateproject/testapp/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+import datetime
+import random
+
+
+# Create your views here.
+
+def results1_view(request):
+    get_date_time = datetime.datetime.now()
+    my_dict = {'get_date_time': get_date_time}
+    return render(request, 'testapp/results1.html', context=my_dict)
+
+
+def results2_view(request):
+    get_date_time = datetime.datetime.now()
+    course = 'Django'
+    prerequesite = 'Python'
+    my_dict = {'get_date_time': get_date_time, 'course': course, 'prerequesite': prerequesite}
+    return render(request, 'testapp/results2.html', context=my_dict)
+
+
+def results3_view(request):
+    msg_list = [
+        'You will get marry soon',
+        'You will get good job',
+        'You will get good wife',
+        'Tomorrow is the gud day to propose ur girl friend'
+    ]
+    names_list = [
+        'Kareena Kapoor',
+        'Deepika padukune',
+        'Alia bhatt',
+        'sunny'
+    ]
+    time = datetime.datetime.now()
+    h = int(time.strftime('%H'))
+    if h < 12:
+        s = 'Gud Morning!'
+    elif h < 16:
+        s = 'Gud Afternoon!'
+    elif h < 21:
+        s = 'Gud Evening!'
+    else:
+        s = 'Gud Night!'
+    msg = random.choice(msg_list)
+    name = random.choice(names_list)
+    my_dict = {'time': time, 'msg': msg, 'name': name, 'wish': s}
+    return render(request, 'testapp/results3.html', context=my_dict)
